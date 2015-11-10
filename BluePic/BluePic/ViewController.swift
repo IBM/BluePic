70109bb6af0d203e05675f75d7303a499bc2b723
Nathan Hekman <hekman.nathan@gmail.com>
2015-11-10 11:22:03 -0600

    created Xcode project

---
+++ BluePic/BluePic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluePic
+//
+//  Created by Nathan Hekman on 11/10/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
