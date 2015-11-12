10409d72cd848a304aabb44eb3b989821688a09c
rolivieri <roliv@us.ibm.com>
2015-11-12 11:34:04 -0600

    Project setup again.

---
+++ BluePic-iOS/BluePic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluePic
+//
+//  Created by Ricardo Olivieri on 11/12/15.
+//  Copyright © 2015 MIL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
