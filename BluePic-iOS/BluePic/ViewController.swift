2535902e067120499e9454de1013ecb18feb8219
Nathan Hekman <hekman.nathan@gmail.com>
2015-11-13 16:31:05 -0600

    imported Alamofire manually

--- BluePic-iOS/BluePic/ViewController.swift
+++ BluePic-iOS/BluePic/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Alamofire.request(.GET, "https://httpbin.org/get")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
