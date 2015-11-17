56d7646e38d8e379f6449c6e94a3ec36666f6d20
Karl Weinmeister <kweinmeister@us.ibm.com>
2015-11-17 14:40:54 -0600

    Fixed integrating CDTDatastore

--- BluePic-iOS/BluePic/Controllers/ViewController.swift
+++ BluePic-iOS/BluePic/Controllers/ViewController.swift
@@ -17,6 +17,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         Alamofire.request(.GET, "https://httpbin.org/get")
         // Do any additional setup after loading the view, typically from a nib.
+
+        //Test code to see if CDTDatastore works
+        do {
+            let fileManager = NSFileManager.defaultManager()
+            
+            let documentsDir = fileManager.URLsForDirectory(.DocumentDirectory,
+                inDomains: .UserDomainMask).last!
+            
+            let storeURL = documentsDir.URLByAppendingPathComponent("cloudant-sync-datastore")
+            let path = storeURL.path
+            
+            let manager = try CDTDatastoreManager(directory: path)
+            let datastore = try manager.datastoreNamed("my_datastore")
+            
+            // Create a document
+            let rev = CDTDocumentRevision(docId: "doc1")
+        } catch {
+            print("Encountered an error: \(error)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
