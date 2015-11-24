4e37069e67428f215ee3a0acdf3034aad1373314
Nathan Hekman <hekman.nathan@gmail.com>
2015-11-24 11:49:52 -0600

    added authenticate method to ObjectStorageDataManager

--- BluePic-iOS/BluePic/DataManagers/ObjectStorageDataManager.swift
+++ BluePic-iOS/BluePic/DataManagers/ObjectStorageDataManager.swift
@@ -38,5 +38,17 @@ class ObjectStorageDataManager: NSObject {
         
     }
     
+    /**
+     * Convenience method for authenticating.
+     */
+    func authenticate(onSuccess: () -> Void, onFailure: (error: String) -> Void) {
+        objectStorageClient.authenticate({() in
+            onSuccess()
+            }, onFailure: {(error) in
+                onFailure(error: error)
+                
+        })
+    }
+    
     
 }
