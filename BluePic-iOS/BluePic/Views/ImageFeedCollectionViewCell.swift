9a3834753267c90597beb31a5b1ff2d855d39a6c
ajbuck@us.ibm.com <ajbuck@us.ibm.com>
2015-12-01 15:46:31 -0600

    Added image view and holder view

---
+++ BluePic-iOS/BluePic/Views/ImageFeedCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageFeedCollectionViewCell.swift
+//  BluePic
+//
+//  Created by Alex Buck on 12/1/15.
+//  Copyright © 2015 MIL. All rights reserved.
+//
+
+import UIKit
+
+class ImageFeedCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
