0a8dc0ccd85b70add14a464fc5f9fc5fb9f81ff5
Nathan Hekman <hekman.nathan@gmail.com>
2015-12-08 13:41:48 -0600

    removed unused method

--- BluePic-iOS/BluePic/DataManagers/ObjectStorageDataManager.swift
+++ BluePic-iOS/BluePic/DataManagers/ObjectStorageDataManager.swift
@@ -22,23 +22,17 @@ class ObjectStorageDataManager: NSObject {
     
     private override init() {
         // Set up connection properties for Object Storage service on Bluemix
-//        let userId = Utils.getKeyFromPlist("keys", key: "obj_stg_user_id")
-//        let password = Utils.getKeyFromPlist("keys", key: "obj_stg_password")
-//        let projectId = Utils.getKeyFromPlist("keys", key: "obj_stg_project_id")
-//        let authURL = Utils.getKeyFromPlist("keys", key: "obj_stg_auth_url")
-//        let publicURL = Utils.getKeyFromPlist("keys", key: "obj_stg_public_url")
-        //self.objectStorageClient = ObjectStorageClient(userId: userId, password: password, projectId: projectId, authURL: authURL, publicURL: publicURL)
+        let userId = Utils.getKeyFromPlist("keys", key: "obj_stg_user_id")
+        let password = Utils.getKeyFromPlist("keys", key: "obj_stg_password")
+        let projectId = Utils.getKeyFromPlist("keys", key: "obj_stg_project_id")
+        let authURL = Utils.getKeyFromPlist("keys", key: "obj_stg_auth_url")
+        let publicURL = Utils.getKeyFromPlist("keys", key: "obj_stg_public_url")
+        self.objectStorageClient = ObjectStorageClient(userId: userId, password: password, projectId: projectId, authURL: authURL, publicURL: publicURL)
     
     }
     
     
     var objectStorageClient: ObjectStorageClient!
-    
-    
-    private func initializeObjectStorageClient() {
-
-        
-    }
     
     
     
