06de522e909dcabfd77184b9bd577186f90f2862
Nathan Hekman <hekman.nathan@gmail.com>
2015-12-08 13:41:29 -0600

    refactored Object Storage wrapper to use the default init

--- BluePic-iOS/BluePic/DataManagers/ObjectStorageDataManager.swift
+++ BluePic-iOS/BluePic/DataManagers/ObjectStorageDataManager.swift
@@ -14,27 +14,29 @@ class ObjectStorageDataManager: NSObject {
     static let SharedInstance: ObjectStorageDataManager = {
         
         var manager = ObjectStorageDataManager()
-        manager.initializeObjectStorageClient()
         
         return manager
         
     }()
     
     
-    private override init() {} //This prevents others from using the default '()' initializer for this class.
+    private override init() {
+        // Set up connection properties for Object Storage service on Bluemix
+//        let userId = Utils.getKeyFromPlist("keys", key: "obj_stg_user_id")
+//        let password = Utils.getKeyFromPlist("keys", key: "obj_stg_password")
+//        let projectId = Utils.getKeyFromPlist("keys", key: "obj_stg_project_id")
+//        let authURL = Utils.getKeyFromPlist("keys", key: "obj_stg_auth_url")
+//        let publicURL = Utils.getKeyFromPlist("keys", key: "obj_stg_public_url")
+        //self.objectStorageClient = ObjectStorageClient(userId: userId, password: password, projectId: projectId, authURL: authURL, publicURL: publicURL)
+    
+    }
     
     
     var objectStorageClient: ObjectStorageClient!
     
     
     private func initializeObjectStorageClient() {
-        // Set up connection properties for Object Storage service on Bluemix
-        let userId = Utils.getKeyFromPlist("keys", key: "obj_stg_user_id")
-        let password = Utils.getKeyFromPlist("keys", key: "obj_stg_password")
-        let projectId = Utils.getKeyFromPlist("keys", key: "obj_stg_project_id")
-        let authURL = Utils.getKeyFromPlist("keys", key: "obj_stg_auth_url")
-        let publicURL = Utils.getKeyFromPlist("keys", key: "obj_stg_public_url")
-        self.objectStorageClient = ObjectStorageClient(userId: userId, password: password, projectId: projectId, authURL: authURL, publicURL: publicURL)
+
         
     }
     
