5853cadbd88c45fa20671d9e723c56d9f26bb9ac
Alex Buck <ajbuck@us.ibm.com>
2015-12-08 20:55:06 -0600

    In the process of building the profile view

--- BluePic-iOS/BluePic/Controllers/ProfileViewController.swift
+++ BluePic-iOS/BluePic/Controllers/ProfileViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    var viewModel : ProfileViewModel!
+    var refreshControl: UIRefreshControl!
+    
+    let kHeaderViewHeight : CGFloat = 480
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        setupViewModel()
+        
+        setupCollectionView()
 
         // Do any additional setup after loading the view.
     }
@@ -20,6 +34,51 @@ class ProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    func setupViewModel(){
+        
+        viewModel = ProfileViewModel(refreshVCCallback: reloadDataInCollectionView)
+        
+        
+    }
+    
+    
+    func setupCollectionView(){
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        Utils.registerSupplementaryElementOfKindNibWithCollectionView("ProfileHeaderCollectionReusableView", kind: UICollectionElementKindSectionHeader, collectionView: collectionView)
+        
+        Utils.registerNibWithCollectionView("ImageFeedCollectionViewCell", collectionView: collectionView)
+        
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.addTarget(self, action: "userTriggeredRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        self.refreshControl.hidden = true
+        
+        self.collectionView.addSubview(refreshControl)
+    }
+    
+    
+    func userTriggeredRefresh(){
+        
+        
+        self.refreshControl.endRefreshing()
+        
+        viewModel.repullForNewData()
+        
+    }
+    
+    
+    func reloadDataInCollectionView(){
+        
+        collectionView.reloadData()
+        
+        
+    }
+    
+    
     
 
     /*
@@ -32,4 +91,89 @@ class ProfileViewController: UIViewController {
     }
     */
 
+}
+
+
+
+extension ProfileViewController: UICollectionViewDataSource {
+    
+    
+    /**
+     Method setups up the viewForSupplementaryElementOfKind by asking the viewModel to set it up. This specifically sets up a header view on the top of the collection view to be clear so you can see the image view at the top of the collection view that is actually below the collection view.
+     
+     - parameter collectionView:
+     - parameter kind:
+     - parameter indexPath:
+     
+     - returns:
+     */
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        return viewModel.setUpSectionHeaderViewForIndexPath(
+            indexPath,
+            kind: kind,
+            collectionView: collectionView
+        )
+    }
+    
+    
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return viewModel.setUpCollectionViewCell(indexPath, collectionView : collectionView)
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsInSection(section)
+    }
+    
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return viewModel.numberOfSectionsInCollectionView()
+    }
+    
+}
+
+
+extension ProfileViewController: UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
+    }
+    
+}
+
+
+extension ProfileViewController: UICollectionViewDelegateFlowLayout {
+    
+    /**
+     Method sets the size of the section header at indexpath depending on what section it is.
+     
+     - parameter collectionView:
+     - parameter collectionViewLayout:
+     - parameter section:
+     
+     - returns:
+     */
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize{
+        
+        let collectionWidth = collectionView.frame.size.width
+        
+        if(section == 0){
+            return CGSizeMake(collectionWidth, kHeaderViewHeight)
+        }
+        else{
+            return CGSizeMake(collectionWidth, 0)
+        }
+        
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        return viewModel.sizeForItemAtIndexPath(indexPath, collectionView: collectionView)
+    }
+    
+    
+    
 }
---
+++ BluePic-iOS/BluePic/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,121 @@
+//
+//  ProfileViewModel.swift
+//  BluePic
+//
+//  Created by Alex Buck on 12/8/15.
+//  Copyright © 2015 MIL. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewModel: NSObject {
+
+    var pictureDataArray = [Picture]()
+    var refreshVCCallback : (()->())!
+    let kNumberOfSectionsInCollectionView = 1
+    
+    init(refreshVCCallback : (()->())){
+       super.init()
+        
+        self.refreshVCCallback  = refreshVCCallback
+        
+        
+        DataManagerCalbackCoordinator.SharedInstance.addCallback(handleDataManagerNotifications)
+        
+        getPictureObjects()
+        
+    }
+    
+
+    
+    func handleDataManagerNotifications(dataManagerNotification : DataManagerNotification){
+        
+    }
+    
+    
+    
+    
+    func getPictureObjects(){
+        pictureDataArray = CloudantSyncClient.SharedInstance.getAllPictureObjectsOfOwnerId(FacebookDataManager.SharedInstance.fbUniqueUserID!)
+        
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.callRefreshCallBack()
+        }
+    }
+    
+    
+    func repullForNewData(){
+        CloudantSyncClient.SharedInstance.pullFromRemoteDatabase()
+    }
+    
+    
+    
+    
+    func numberOfSectionsInCollectionView() -> Int {
+        return kNumberOfSectionsInCollectionView
+    }
+    
+    
+    func numberOfItemsInSection(section : Int) -> Int {
+        return pictureDataArray.count
+    }
+    
+    func sizeForItemAtIndexPath(indexPath : NSIndexPath, collectionView : UICollectionView) -> CGSize {
+        
+        return CGSize(width: collectionView.frame.width, height: 364)
+    }
+    
+    
+    
+    func setUpCollectionViewCell(indexPath : NSIndexPath, collectionView : UICollectionView) -> UICollectionViewCell {
+        
+        let cell: ImageFeedCollectionViewCell
+        
+        cell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageFeedCollectionViewCell", forIndexPath: indexPath) as! ImageFeedCollectionViewCell
+        
+        let picture = pictureDataArray[indexPath.row]
+        
+        cell.setupData(picture.url, displayName: picture.displayName, ownerName: picture.ownerName, timeStamp: picture.timeStamp)
+        
+        cell.layer.shouldRasterize = true
+        cell.layer.rasterizationScale = UIScreen.mainScreen().scale
+        
+        return cell
+        
+    }
+    
+    
+    /**
+     Method sets up the section header for the indexPath parameter
+     
+     - parameter indexPath:      NSIndexPath
+     - parameter kind:           String
+     - parameter collectionView: UICollectionView
+     
+     - returns: TripDetailSupplementaryView
+     */
+    func setUpSectionHeaderViewForIndexPath(indexPath : NSIndexPath, kind: String, collectionView : UICollectionView) -> ProfileHeaderCollectionReusableView {
+        
+        let header : ProfileHeaderCollectionReusableView
+        
+        header = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "ProfileHeaderCollectionReusableView", forIndexPath: indexPath) as! ProfileHeaderCollectionReusableView
+        
+        header.setupData(FacebookDataManager.SharedInstance.fbUserDisplayName, numberOfShots: pictureDataArray.count)
+        
+        return header
+    }
+    
+    
+    
+
+    
+    func callRefreshCallBack(){
+        if let callback = refreshVCCallback {
+            callback()
+        }
+    }
+    
+    
+    
+}
