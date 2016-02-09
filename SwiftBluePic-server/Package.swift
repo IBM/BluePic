f71002d7914db6dd450b2841e6aa53fb5b7b9031
Shmuel Kallner <kallner@il.ibm.com>
2016-02-09 20:07:10 +0200

    Added Kitura-redis as a dependent

--- SwiftBluePic-server/Package.swift
+++ SwiftBluePic-server/Package.swift
@@ -12,7 +12,8 @@ let package = Package(
     name: "SwiftBluePic-server",
           dependencies: [
                .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", majorVersion: 1),
-               .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1)
+               .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1),
+               .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 1)
           ]
 )
 
