ab7b5680dda9bfbd3fe254545760e239f15d6483
Shmuel Kallner <kallner@il.ibm.com>
2016-02-16 20:39:54 -0500

    Changed dependency URLs to use SSH until we can use HTTPS

--- SwiftBluePic-server/Package.swift
+++ SwiftBluePic-server/Package.swift
@@ -1,19 +1,12 @@
-//
-//  Package.swift
-//  PhoenixSample
-//
-//  Created by Daniel Firsht on 12/10/15.
-//  Copyright © 2015 Daniel Firsht. All rights reserved.
-//
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftBluePic-server",
           dependencies: [
-               .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", majorVersion: 0),
-               .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0),
-               .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", majorVersion: 0)
+               .Package(url: "git@github.com:IBM-Swift/Kitura-router.git", majorVersion: 0),
+               .Package(url: "git@github.com:IBM-Swift/Kitura-CouchDB.git", majorVersion: 0),
+               .Package(url: "git@github.com:IBM-Swift/Kitura-redis.git", majorVersion: 0),
           ]
 )
 
