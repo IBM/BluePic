14e66dbf40dfe9a96089200cccedb78c582419cf
Robert F. Dickerson <rfdickerson@gmail.com>
2016-02-29 15:43:21 -0600

    Updated dependencies to develop versions
    IBM-Swift/Kitura#200

--- KituraBluePic-server/Package.swift
+++ KituraBluePic-server/Package.swift
@@ -19,9 +19,9 @@ import PackageDescription
 let package = Package(
     name: "KituraBluePic-server",
           dependencies: [
-               .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,2,0)..<Version(0,3,0)),
-               .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", versions: Version(0,2,0)..<Version(0,3,0)),
-               .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", versions: Version(0,2,0)..<Version(0,3,0)),
+               .Package(url: "https://github.com/IBM-Swift/Kitura-router.git", versions: Version(0,3,0)..<Version(0,4,0)),
+               .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", versions: Version(0,3,0)..<Version(0,4,0)),
+               .Package(url: "https://github.com/IBM-Swift/Kitura-redis.git", versions: Version(0,3,0)..<Version(0,4,0)),
           ]
 )
 
