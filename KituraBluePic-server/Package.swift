2899ae218dee7130edaf9eb6b418eb7edf2dd1e0
rolivieri <roliv@us.ibm.com>
2016-04-15 11:51:08 -0500

    Project restructuring in progress.

--- KituraBluePic-server/Package.swift
+++ KituraBluePic-server/Package.swift
@@ -17,7 +17,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "KituraBluePic-server",
+    name: "BluePic-server",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 7),
