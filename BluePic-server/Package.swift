622794e5c7f6f9879bfe0bc3733f769a7fb2a4a1
rolivieri <roliv@us.ibm.com>
2016-04-15 22:42:30 -0500

    Updated Package.swift - removed references to unused packages.

--- BluePic-server/Package.swift
+++ BluePic-server/Package.swift
@@ -21,8 +21,7 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebookToken.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsGoogleToken.git", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebookToken.git", majorVersion: 0, minor: 7)
     ],
     exclude: ["Makefile", "Kitura-Build"]
 )
