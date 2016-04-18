c50ff12aa32fa7515952eea2cf7a90b07af1b1f2
rolivieri <roliv@us.ibm.com>
2016-04-18 15:40:27 -0500

    Renaming BluePic-server instances to BluePic-Server.

--- BluePic-server/Package.swift
+++ BluePic-server/Package.swift
@@ -17,7 +17,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "BluePic-server",
+    name: "BluePic-Server",
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 7),
