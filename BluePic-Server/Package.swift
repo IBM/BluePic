8bdc183c882462a76e4bb45302d076e7c7fafafa
rolivieri <roliv@us.ibm.com>
2016-04-19 15:11:05 -0500

    Updated Package.swift to include SDK for Object Storage (v0.0.22).

--- BluePic-Server/Package.swift
+++ BluePic-Server/Package.swift
@@ -23,6 +23,7 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/Kitura-CredentialsFacebookToken.git", majorVersion: 0, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 0),
+        .Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-clientsdk-swift-objectstore.git", versions: Version(0,0,0)..<Version(0,0,23))
     ],
     exclude: ["Makefile", "Kitura-Build"]
 )
