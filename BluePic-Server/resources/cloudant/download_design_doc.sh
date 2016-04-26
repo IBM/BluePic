00ce3e481eb08f17ab7e4f2cc03e89cdd7420bc1
rolivieri <roliv@us.ibm.com>
2016-04-26 14:16:32 -0500

    More updates to automation scripts.

--- BluePic-Server/resources/cloudant/download_design_doc.sh
+++ BluePic-Server/resources/cloudant/download_design_doc.sh
@@ -20,3 +20,5 @@
 set -e
 
 curl -X GET "https://d60741e4-629e-48e4-aa5d-da6e7557d5b5-bluemix.cloudant.com/bluepic_db/_design/main_design" -u d60741e4-629e-48e4-aa5d-da6e7557d5b5-bluemix:f2c7c6a70784057bf8ab86a3ae5c9ac7129fedf67619f243eb030058764792e3 > main_design.json
+
+curl -X GET "https://455a9bcb-1b79-4da9-a2aa-3babd429db1f-bluemix.cloudant.com/attendee/_design/attendee_main_design" -u 455a9bcb-1b79-4da9-a2aa-3babd429db1f-bluemix:e20b951230d1eb08e414eb373c0ee8ff368b33534440ebd556dde32af88eb40e > attendee_main_design.json
