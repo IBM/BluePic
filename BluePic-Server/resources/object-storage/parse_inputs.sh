2d2df8b512a5794070cad698a41d24c277e87d64
rolivieri <roliv@us.ibm.com>
2016-04-27 16:34:17 -0500

    Updating automation scripts for object storage.

---
+++ BluePic-Server/resources/object-storage/parse_inputs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+##
+# Copyright IBM Corporation 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+for i in "$@"
+do
+case $i in
+    -u=*|--userid=*)
+    userid="${i#*=}"
+    shift # past argument=value
+    ;;
+    -p=*|--password=*)
+    password="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+        # unknown option
+    ;;
+esac
+done
+
+# Variables (tmp)
+userid=69435710c71c4ed2b78856520f9cae25
+password=F*nt4gmY-Oss4{v.
+
+echo "Variables:"
+echo -e "  userid: $userid"
+#echo -e "  database: $database"
---
+++ BluePic-Server/resources/object-storage/populate_container.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##
+# Copyright IBM Corporation 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+# If any commands fail, we want the shell script to exit immediately.
+set -e
+
+#https://<access point>/<API version>/AUTH_<project ID>/<container namespace>/<object namespace>
+#https://dal.objectstorage.open.softlayer.com/v1/AUTH_742fffae2c24438b83a2c43491119a82
+
+# Parse input parameters
+source ./parse_inputs.sh
+
+# Variables
+authUrl=https://identity.open.softlayer.com/v3/auth/tokens
+
+# Get access token
+authToken=`curl -i -H "Content-Type: application/json" -d "{ \"auth\": { \"identity\": { \"methods\": [ \"password\" ], \"password\": { \"user\": { \"id\": \"$userid\", \"password\": \"$password\" } } }, \"scope\": { \"project\": { \"id\": \"742fffae2c24438b83a2c43491119a82\" } } } }" $authUrl | grep X-Subject-Token | awk '{print $2}' | tr -cd '[[:alnum:]]._-'`
+#echo "authToken: $authToken"
