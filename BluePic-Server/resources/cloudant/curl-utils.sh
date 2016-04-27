1ae8f9d14fb4e85968cd0a8f08b072f1d3eb7bd8
rolivieri <roliv@us.ibm.com>
2016-04-27 14:01:13 -0500

    Updating automation scripts for cloudant.

---
+++ BluePic-Server/resources/cloudant/curl-utils.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+##
+# Copyright IBM Corporation 2016
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##
+
+# If any commands fail, we want the shell script to exit immediately.
+set -e
+
+imgsFolder=`dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )`
+echo "imgsFolder: $imgsFolder"
+
+# Upload images via Kitura-based server
+curl -v --data-binary @$imgsFolder/imgs/tombstone.jpg -X POST http://localhost:8090/users/1000/images/tombstone.jpg/Tombstone
+curl -v --data-binary @$imgsFolder/imgs/swift.png -X POST http://localhost:8090/users/1000/images/swift.png/SwiftRocks
