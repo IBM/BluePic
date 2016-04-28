1b8f1493de530c938078500d46cc02e73f1f993e
Alex Buck <ajbuck@us.ibm.com>
2016-04-28 01:55:03 -0500

    updated scripts, and added some header search paths to make server executable compile

--- server-runner.sh
+++ server-runner.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 set -e 
-cd "$(dirname "$0")" 
-cd BluePic-Server 
-brew install http-parser curl 
-make clean 
-make 
-
-if kill `ps aux | grep -F '.build/debug/Server' | grep -v -F 'grep' | awk '{ print $2 }'`; then
-	echo 'Server was already running, killing server and then starting it back up again'
-	.build/debug/Server &
-else
-	echo 'Server wasnt already running, starting it up'
-	.build/debug/Server &
+if [[ "$1" != "xcode" ]]; then
+	brew install http-parser curl
 fi
+./compile-server.sh
+./run-server.sh $1
