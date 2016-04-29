9320ee614a8549750ae6de617d115bfe1cdf44f7
rolivieri <roliv@us.ibm.com>
2016-04-28 22:51:35 -0500

    Updates to automattion script for object storage.

--- BluePic-Server/resources/object-storage/populate_container.sh
+++ BluePic-Server/resources/object-storage/populate_container.sh
@@ -29,26 +29,48 @@ source ./parse_inputs.sh
 authUrl=https://identity.open.softlayer.com/v3/auth/tokens
 accessPoint=dal.objectstorage.open.softlayer.com
 publicUrl=https://$accessPoint/v1/AUTH_$projectid
-container=9012390
+
+# Containers
+container1=100002031687931a
+container2=100002031687932b
+container3=100002031687933c
 
 # Echo publicUrl
-echo "publicUrl: $publicUrl" 
+echo "publicUrl: $publicUrl"
 
 # Get access token
 authToken=`curl -i -H "Content-Type: application/json" -d "{ \"auth\": { \"identity\": { \"methods\": [ \"password\" ], \"password\": { \"user\": { \"id\": \"$userid\", \"password\": \"$password\" } } }, \"scope\": { \"project\": { \"id\": \"$projectid\" } } } }" $authUrl | grep X-Subject-Token | awk '{print $2}' | tr -cd '[[:alnum:]]._-'`
 
-# Create container
-curl -i $publicUrl/$container -X PUT -H "Content-Length: 0" -H "X-Auth-Token: $authToken"
+# Create and configure containers
+declare -a containers=($container1 $container2 $container3)
 
-# Configure container for web hosting
-curl -i $publicUrl/$container -X POST -H "Content-Length: 0" -H "X-Auth-Token: $authToken" -H  "X-Container-Meta-Web-Listings: true"
+for container in "${containers[@]}"; do
+  echo "container: $container"
+  # Create container
+  curl -i $publicUrl/$container -X PUT -H "Content-Length: 0" -H "X-Auth-Token: $authToken"
 
-# Configure container for public access
-curl -i $publicUrl/$container -X POST -H "Content-Length: 0" -H "X-Auth-Token: $authToken" -H  "X-Container-Read: .r:*,.rlistings"
+  # Configure container for web hosting
+  curl -i $publicUrl/$container -X POST -H "Content-Length: 0" -H "X-Auth-Token: $authToken" -H  "X-Container-Meta-Web-Listings: true"
 
-# Upload image to container
+  # Configure container for public access
+  curl -i $publicUrl/$container -X POST -H "Content-Length: 0" -H "X-Auth-Token: $authToken" -H  "X-Container-Read: .r:*,.rlistings"
+done
+
+# Upload image sto containers
 imagesFolder=`dirname $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )`/images
-curl -i $publicUrl/$container/rush.jpg --data-binary @$imagesFolder/rush.jpg -X PUT -H "Content-Type: image/jpeg" -H "X-Auth-Token: $authToken"
+echo "imagesFolder: $imagesFolder"
+declare -a images=("$container1:rush.jpg:image/jpg" "$container2:rush.jpg:image/jpg" "$container3:rush.jpg:image/jpg")
+
+for record in "${images[@]}"; do
+  IFS=':' read -ra image <<< "$record"
+  container=${image[0]}
+  fileName=${image[1]}
+  contentType=${image[2]}
+  echo "container: $container"
+  echo "fileName: $fileName"
+  echo "contentType: $contentType"
+  curl -i $publicUrl/$container/$fileName --data-binary @$imagesFolder/$fileName -X PUT -H "Content-Type: $contentType" -H "X-Auth-Token: $authToken"
+done
 
 echo
 echo "Successfully finished populating object storage."
