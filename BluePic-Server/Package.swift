1fece01d11aa30dede6a13738928ea58d372fb28
rolivieri <roliv@us.ibm.com>
2016-05-05 15:35:24 -0500

    Upgraded to latest version of the Object Storage SDK.

--- BluePic-Server/Package.swift
+++ BluePic-Server/Package.swift
@@ -22,8 +22,8 @@ let package = Package(
       .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 11),
       .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 11),
       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 0),
-      .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstore-swift-sdk.git", majorVersion: 0, minor: 1)
-      //.Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-clientsdk-swift-objectstore.git", versions: Version(0,0,0)..<Version(0,0,23))
+      .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-swift-sdk.git", majorVersion: 0, minor: 2)
+      //.Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-swift-sdk.git", versions: Version(0,0,0)..<Version(0,0,23))
     ],
     exclude: ["Makefile", "Kitura-Build"]
 )
