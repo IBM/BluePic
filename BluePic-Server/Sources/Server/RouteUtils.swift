59d17ea3d32d0ecb017f35d167d2a2cf9007f435
rolivieri <roliv@us.ibm.com>
2016-05-05 15:35:24 -0500

    Upgraded to latest version of the Object Storage SDK.

--- BluePic-Server/Sources/Server/RouteUtils.swift
+++ BluePic-Server/Sources/Server/RouteUtils.swift
@@ -20,7 +20,7 @@ import Kitura
 import KituraNet
 import LoggerAPI
 import SwiftyJSON
-import BluemixObjectStore
+import BluemixObjectStorage
 
 /**
 * This method should kick off asynchronously an OpenWhisk sequence
@@ -122,13 +122,13 @@ func generateInternalError() -> NSError {
 func generateUrl(forContainer containerName: String, forImage imageName: String) -> String {
   //let url = "http://\(database.connProperties.host):\(database.connProperties.port)/\(database.name)/\(imageId)/\(attachmentName)"
   //let url = "\(config.appEnv.url)/images/\(imageId)/\(attachmentName)"
-  let url = "\(objStoreConnProps.publicURL)/\(containerName)/\(imageName)"
+  let url = "\(objStorageConnProps.publicURL)/\(containerName)/\(imageName)"
   return url
 }
 
 func createContainer(withName name: String, completionHandler: (success: Bool) -> Void) {
   // Cofigure container for public access and web hosting
-  let configureContainer = { (container: ObjectStoreContainer) -> Void in
+  let configureContainer = { (container: ObjectStorageContainer) -> Void in
     let metadata:Dictionary<String, String> = ["X-Container-Meta-Web-Listings" : "true", "X-Container-Read" : ".r:*,.rlistings"]
     container.updateMetadata(metadata: metadata) { (error) in
       if let _ = error {
@@ -142,7 +142,7 @@ func createContainer(withName name: String, completionHandler: (success: Bool) -
   }
 
   // Create container
-  let createContainer = { (objStore: ObjectStore?) -> Void in
+  let createContainer = { (objStore: ObjectStorage?) -> Void in
     if let objStore = objStore {
       objStore.createContainer(name: name) { (error, container) in
         if let container = container where error == nil {
@@ -159,12 +159,12 @@ func createContainer(withName name: String, completionHandler: (success: Bool) -
   }
 
   // Connect, create, and configure container
-  connectToObjectStore(completionHandler: createContainer)
+  connectToObjectStorage(completionHandler: createContainer)
 }
 
 func store(image: NSData, withName name: String, inContainer containerName: String, completionHandler: (success: Bool) -> Void) {
   // Store image in container
-  let storeImage = { (container: ObjectStoreContainer) -> Void in
+  let storeImage = { (container: ObjectStorageContainer) -> Void in
     container.storeObject(name: name, data: image) { (error, object) in
       if let _ = error {
         Log.error("Could not save image named '\(name)' in container.")
@@ -177,7 +177,7 @@ func store(image: NSData, withName name: String, inContainer containerName: Stri
   }
 
   // Get reference to container
-  let retrieveContainer = { (objStore: ObjectStore?) -> Void in
+  let retrieveContainer = { (objStore: ObjectStorage?) -> Void in
     if let objStore = objStore {
       objStore.retrieveContainer(name: containerName) { (error, container) in
         if let container = container where error == nil {
@@ -193,7 +193,7 @@ func store(image: NSData, withName name: String, inContainer containerName: Stri
   }
 
   // Connect, create, and configure container
-  connectToObjectStore(completionHandler: retrieveContainer)
+  connectToObjectStorage(completionHandler: retrieveContainer)
 }
 
 private func massageImageRecord(containerName: String, record: inout JSON) {
@@ -223,10 +223,10 @@ private func constructDocument(records: [JSON]) -> JSON {
   return jsonDocument
 }
 
-private func connectToObjectStore(completionHandler: (objStore: ObjectStore?) -> Void) {
+private func connectToObjectStorage(completionHandler: (objStore: ObjectStorage?) -> Void) {
   // Create object store instance and connect
-  let objStore = ObjectStore(projectId: objStoreConnProps.projectId)
-  objStore.connect(userId: objStoreConnProps.userId, password: objStoreConnProps.password, region: ObjectStore.REGION_DALLAS) { (error) in
+  let objStore = ObjectStorage(projectId: objStorageConnProps.projectId)
+  objStore.connect(userId: objStorageConnProps.userId, password: objStorageConnProps.password, region: ObjectStorage.REGION_DALLAS) { (error) in
     if let error = error {
       let errorMsg = "Could not connect to Object Storage."
       Log.error("\(errorMsg) Error was: '\(error)'.")
