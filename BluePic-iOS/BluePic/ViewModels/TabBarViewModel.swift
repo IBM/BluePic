394dc970e85caf7250b37ce6ca8b23b3aa0a4a4d
Alex Buck <ajbuck@us.ibm.com>
2016-05-07 09:51:13 -0500

    Cleaned up Profile and tab bar

--- BluePic-iOS/BluePic/ViewModels/TabBarViewModel.swift
+++ BluePic-iOS/BluePic/ViewModels/TabBarViewModel.swift
@@ -29,9 +29,6 @@ class TabBarViewModel: NSObject {
     //callback that allows the tab bar view model to send DataManagerNotifications to the tab bar VC
     var notifyTabBarVC: ((tabBarViewModelNotification: TabBarViewModelNotification)->())!
     
-    //state variable that keeps track of if we've successfully pulled data yet, used to help make sure we are showing the loading animation at the right time
-    var hasSuccessFullyPulled = false
-    
     
     /**
      Method called upon init, it sets up the callback
@@ -77,8 +74,5 @@ class TabBarViewModel: NSObject {
 
     }
     
-   
-    
-
-  
+ 
 }
