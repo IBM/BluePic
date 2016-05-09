a8237cb672005af9f99caddb7a93a459e6ec60ae
Taylor Franklin <tfranklin@us.ibm.com>
2016-05-09 17:31:27 -0500

    finished most of the UI for the search view controller #36

--- BluePic-iOS/BluePic/Controllers/SearchViewController.swift
+++ BluePic-iOS/BluePic/Controllers/SearchViewController.swift
@@ -16,12 +16,23 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    let tempPopularTags = ["MOUNTAIN", "TREES", "SKY", "NATURE", "PEOPLE", "OCEAN", "CITY"]
     @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var tagsButton: UIButton!
+    @IBOutlet weak var tagCollectionView: UICollectionView!
+    
+    let kCellPadding: CGFloat = 60
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let attributedString = NSMutableAttributedString(string: "TAGS")
+        attributedString.addAttribute(NSKernAttributeName, value: CGFloat(1.7), range: NSRange(location: 0, length: attributedString.length))
+        tagsButton.titleLabel!.attributedText = attributedString
+        
+        Utils.registerNibWithCollectionView("TagCollectionViewCell", collectionView: tagCollectionView)
+
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -34,8 +45,37 @@ class SearchViewController: UIViewController {
     }
 
     @IBAction func popVC(sender: AnyObject) {
+        searchField.resignFirstResponder()
         self.navigationController?.popViewControllerAnimated(true)
     }
+}
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tempPopularTags.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TagCollectionViewCell", forIndexPath: indexPath) as? TagCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.tagLabel.text = tempPopularTags[indexPath.item]
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let size = NSString(string: tempPopularTags[indexPath.item]).sizeWithAttributes(nil)
+        return CGSizeMake(size.width + kCellPadding, 30.0)
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        collectionView.deselectItemAtIndexPath(indexPath, animated: true)
+        // open feed of items with selected tag
+    }
+    
 }
 
 extension SearchViewController: UITextFieldDelegate {
