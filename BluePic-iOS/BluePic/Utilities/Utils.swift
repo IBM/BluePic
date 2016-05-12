9712432c2efa444a8dda6aad29bfde6f00c88c75
Alex Buck <ajbuck@us.ibm.com>
2016-05-12 12:14:23 -0500

    Finished formatting gps string

--- BluePic-iOS/BluePic/Utilities/Utils.swift
+++ BluePic-iOS/BluePic/Utilities/Utils.swift
@@ -185,4 +185,27 @@ class Utils: NSObject {
         
     }
     
+    
+    class func coordinateString(latitude:Double, longitude:Double) -> String {
+        var latSeconds = Int(latitude * 3600)
+        let latDegrees = latSeconds / 3600
+        latSeconds = abs(latSeconds % 3600)
+        let latMinutes = latSeconds / 60
+        latSeconds %= 60
+        var longSeconds = Int(longitude * 3600)
+        let longDegrees = longSeconds / 3600
+        longSeconds = abs(longSeconds % 3600)
+        let longMinutes = longSeconds / 60
+        longSeconds %= 60
+        return String(format:"%d° %d' %d\" %@, %d° %d' %d\" %@",
+                      abs(latDegrees),
+                      latMinutes,
+                      latSeconds,
+                      {return latDegrees >= 0 ? "N" : "S"}(),
+                      abs(longDegrees),
+                      longMinutes,
+                      longSeconds,
+                      {return longDegrees >= 0 ? "E" : "W"}() )
+    }
+    
 }
