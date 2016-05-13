78e79fc9c59aadf5fd5aad6cc5902c13fa2d2936
Taylor Franklin <tfranklin@us.ibm.com>
2016-05-13 11:52:11 -0500

    finished updating server to 5/3 binaries #75

--- BluePic-Server/Package.swift
+++ BluePic-Server/Package.swift
@@ -23,7 +23,6 @@ let package = Package(
       .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 13),
       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1),
       .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-swift-sdk.git", majorVersion: 0, minor: 3)
-      //.Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-swift-sdk.git", versions: Version(0,0,0)..<Version(0,0,23))
     ],
     exclude: ["Makefile", "Kitura-Build"]
 )
