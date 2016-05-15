883b564ed89410ec622cec506b6b6834411240e9
rolivieri <roliv@us.ibm.com>
2016-05-15 00:11:27 -0500

    Adding logic for searching images by tag; updated cloudant design document.

--- BluePic-Server/resources/cloudant/query_views.sh
+++ BluePic-Server/resources/cloudant/query_views.sh
@@ -55,4 +55,9 @@ curl -g -X GET "https://$username.cloudant.com/$database/_design/main_design/_vi
 echo
 echo
 
+echo "Querying images_by_tags view"
+curl -g -X GET "https://$username.cloudant.com/$database/_design/main_design/_view/images_by_tags?group=true&group_level=1" -u $username:$password
+echo
+echo
+
 echo "Successfully finished querying views on cloudant database '$database'."
