6939c552af12725303da187acdb030b261d96798
Taylor Franklin <tfranklin@us.ibm.com>
2016-05-20 12:08:07 -0500

    updated copyrights in iOS client #98

--- BluePic-iOS/BluePic/AppDelegate.swift
+++ BluePic-iOS/BluePic/AppDelegate.swift
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corporation 2015
+ * Copyright IBM Corporation 2016
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,10 @@
  * limitations under the License.
  **/
 
-
 import UIKit
 import BMSCore
 import BMSSecurity
 import BMSPush
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
