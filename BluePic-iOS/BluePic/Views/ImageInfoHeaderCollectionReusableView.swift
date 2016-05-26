54490350e11b4494108c6455b6108c73ef2c4b52
Alex Buck <ajbuck@us.ibm.com>
2016-05-26 11:52:01 -0500

    Commented all views

--- BluePic-iOS/BluePic/Views/ImageInfoHeaderCollectionReusableView.swift
+++ BluePic-iOS/BluePic/Views/ImageInfoHeaderCollectionReusableView.swift
@@ -18,23 +18,52 @@ import UIKit
 
 class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
 
+    //label that shows the caption of the image
     @IBOutlet weak var captionLabel: UILabel!
+    
+    //label that shows who the image is by - its shows the users name
     @IBOutlet weak var byUserLabel: UILabel!
+    
+    //label that shows the city and state of where the image was uploaded
     @IBOutlet weak var cityStateLabel: UILabel!
+    
+    //label that shows the coordinates of where the image was uploaded
     @IBOutlet weak var coordinatesLabel: UILabel!
+    
+    //label that shows the date the image was taken on
     @IBOutlet weak var dateLabel: UILabel!
+    
+    //label that shows the time of day the image was taken at
     @IBOutlet weak var timeLabel: UILabel!
+    
+    //imageView that shows a weather icon image for type of weather at the time the image was uploaded
     @IBOutlet weak var weatherImageView: UIImageView!
+    
+    //label that shows the temperature at the time the image was uploaded
     @IBOutlet weak var temperatureLabel: UILabel!
+    
+    //label that says "Tags"
     @IBOutlet weak var tagsLabel: UILabel!
     
+    //constant that defines the captionLabel letter spacing
     private let kCaptionLabelLetterSpacing : CGFloat = 1.7
+    
+    //constant that defines the captionLabel line spacing
     private let kCaptionLabelLineSpacing : CGFloat = 10.0
     
+    //prefix used to get the correct name of the weather icon from the image asset folder
     private let kWeatherIconNamePrefix = "weather_icon_"
+    
+    //degree string used for the temperature
     private let kDegreeSymbolString = "°"
+    
+    //prefix string used for the byUserLabel text
     private let kByUserLabelPrefixString = NSLocalizedString("by", comment: "")
+    
+    //prefix for the dateLabel text
     private let kDateLabelPrefixString = NSLocalizedString("on", comment: "")
+    
+    //prefix for the timeLabel text
     private let kTimeLabelPrefixString = NSLocalizedString("at", comment: "")
     
     
@@ -45,6 +74,19 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
         super.awakeFromNib()
     }
 
+    /**
+     Method sets up the UI with data
+     
+     - parameter caption:       String?
+     - parameter userFullName:  String?
+     - parameter locationName:  String?
+     - parameter latitude:      String?
+     - parameter longitude:     String?
+     - parameter timeStamp:     NSDate?
+     - parameter weatherIconId: Int?
+     - parameter temperature:   Int?
+     - parameter tags:          [Tag]?
+     */
     func setupWithData(caption : String?, userFullName : String?, locationName : String?, latitude : String?, longitude : String?, timeStamp : NSDate?, weatherIconId : Int?, temperature : Int?, tags : [Tag]?){
   
         //setup captionLabel
@@ -57,7 +99,7 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
         setupCityAndStateLabel(locationName)
         
         //setup coordinatesLabel
-        setupCoordintesLabel(latitude, longitude: longitude)
+        setupCoordinatesLabel(latitude, longitude: longitude)
         
         //setup dateLabel
         setupDateLabelWithData(timeStamp)
@@ -72,7 +114,11 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
         
     }
 
-    
+    /**
+     Method sets up the caption label with data
+     
+     - parameter caption: String?
+     */
     private func setupCaptionLabelWithData(caption : String?){
         
         if let imageCaption = caption {
@@ -88,17 +134,31 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
         }
     }
     
-    
+    /**
+     Method sets the user label with data
+     
+     - parameter userFullName: String?
+     */
     private func setupByUserLabel(userFullName : String?){
         let fullName = userFullName ?? ""
         byUserLabel.text = kByUserLabelPrefixString + " " + fullName
     }
     
+    /**
+     Method sets the cityAndStateLabel with data
+     
+     - parameter locationName: String?
+     */
     private func setupCityAndStateLabel(locationName : String?){
         cityStateLabel.text = locationName ?? ""
     }
     
     
+    /**
+     Method sets up the date label with data
+     
+     - parameter timeStamp: NSDate?
+     */
     private func setupDateLabelWithData(timeStamp : NSDate?){
         
         if let date = timeStamp {
@@ -116,7 +176,11 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
         
     }
     
-    
+    /**
+     Method sets up the time label with data
+     
+     - parameter timeStamp: NSDate?
+     */
     private func setupTimeLabelWithData(timeStamp : NSDate?){
         
         if let date = timeStamp {
@@ -134,8 +198,13 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
         
     }
     
-    
-    private func setupCoordintesLabel(latitude : String?, longitude : String?){
+    /**
+     Method sets the coordinatesLabel with data
+     
+     - parameter latitude:  String?
+     - parameter longitude: String?
+     */
+    private func setupCoordinatesLabel(latitude : String?, longitude : String?){
         
         if let latit = latitude,
             let longit = longitude,
@@ -152,7 +221,12 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
         }
     }
 
-    
+    /**
+     Method sets up the weather image view and temperature label with data
+     
+     - parameter weatherIconId: Int?
+     - parameter temperature:   Int?
+     */
     private func setupWeatherImageViewAndTemperatureLabel(weatherIconId : Int?, temperature : Int?){
         
         if let iconId = weatherIconId {
@@ -173,9 +247,13 @@ class ImageInfoHeaderCollectionReusableView: UICollectionReusableView{
                 temperatureLabel.text = "\(temperature)" + kDegreeSymbolString + "C"
             }
         }
-        
     }
     
+    /**
+     Method sets up the tags label
+     
+     - parameter tags: [Tag]?
+     */
     private func setupTagsLabel(tags : [Tag]?){
         
         if let tags = tags {
