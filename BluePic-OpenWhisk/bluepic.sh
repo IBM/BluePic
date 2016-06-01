d91cb3a385db706758f4d57997c091758c6c2ba7
ANDREW M. TRICE <amtrice@us.ibm.com>
2016-05-31 21:29:45 -0400

    implemented callback to kitura to trigger push notification request, extended timeouts for all actions

--- BluePic-OpenWhisk/bluepic.sh
+++ BluePic-OpenWhisk/bluepic.sh
@@ -46,32 +46,36 @@ function install() {
     -p cloudantHost $CLOUDANT_host\
     -p cloudantUsername $CLOUDANT_username\
     -p cloudantPassword $CLOUDANT_password\
-    -p cloudantDbName $CLOUDANT_db
+    -p cloudantDbName $CLOUDANT_db\
+    -p mcaClientId $MCA_client\
+    -p mcaSecret $MCA_secret\
+    -p kituraHost $KITURA_host
 
   echo "Creating actions"
   #this is just a test action to make sure we can make HTTP requests leveraging kitura networking
-  wsk action create --kind swift:3 bluepic/prepareReadImage actions/PrepareToReadImage.swift
-  wsk action create --kind swift:3 bluepic/prepareWeatherRequest actions/PrepareWeatherRequest.swift
-  wsk action create --kind swift:3 bluepic/prepareCloudantWrite actions/PrepareToWriteImage.swift
-  wsk action create --kind swift:3 bluepic/httpGet actions/HttpGet.swift
-  wsk action create --kind swift:3 bluepic/weather actions/Weather.swift
-  wsk action create --kind swift:3 bluepic/alchemy actions/Alchemy.swift
-  wsk action create --kind swift:3 bluepic/cloudantRead actions/CloudantRead.swift
-  wsk action create --kind swift:3 bluepic/cloudantWrite actions/CloudantWrite.swift
-  wsk action create --kind swift:3 bluepic/processImageStub actions/Stub.swift
+  wsk action create --kind swift:3 bluepic/prepareReadImage actions/PrepareToReadImage.swift -t 300000
+  wsk action create --kind swift:3 bluepic/prepareWeatherRequest actions/PrepareWeatherRequest.swift -t 300000
+  wsk action create --kind swift:3 bluepic/prepareCloudantWrite actions/PrepareToWriteImage.swift -t 300000
+  wsk action create --kind swift:3 bluepic/httpGet actions/HttpGet.swift -t 300000
+  wsk action create --kind swift:3 bluepic/weather actions/Weather.swift -t 300000
+  wsk action create --kind swift:3 bluepic/alchemy actions/Alchemy.swift -t 300000
+  wsk action create --kind swift:3 bluepic/cloudantRead actions/CloudantRead.swift -t 300000
+  wsk action create --kind swift:3 bluepic/cloudantWrite actions/CloudantWrite.swift -t 300000
+  wsk action create --kind swift:3 bluepic/processImageStub actions/Stub.swift -t 300000
+  wsk action create --kind swift:3 bluepic/kituraCallback actions/KituraCallback.swift -t 300000
   
   
   #create sequences to tie everything together - lots of these are for testing
-  wsk action create bluepic/processImage --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy,bluepic/prepareCloudantWrite,bluepic/cloudantWrite
-  wsk action create bluepic/processRequestThroughCloudantWrite --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy,bluepic/prepareCloudantWrite,bluepic/cloudantWrite
-  wsk action create bluepic/processRequestToCloudantWrite --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy,bluepic/prepareCloudantWrite
-  wsk action create bluepic/processRequestThroughAlchemy --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy
-  wsk action create bluepic/processRequestThroughWeather --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather
-  wsk action create bluepic/processRequestToWeather --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest
-  wsk action create bluepic/processRequestThroughReadImage --sequence bluepic/prepareReadImage,bluepic/cloudantRead
-  wsk action create bluepic/processRequestToReadImage --sequence bluepic/prepareReadImage
-  wsk action create bluepic/processRequestThroughReadUser --sequence bluepic/prepareReadUser,bluepic/cloudantRead
-  wsk action create bluepic/processFinalWrite --sequence bluepic/prepareCloudantWrite,bluepic/cloudantWrite
+  wsk action create bluepic/processImage --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy,bluepic/prepareCloudantWrite,bluepic/cloudantWrite,bluepic/kituraCallback -t 300000
+  wsk action create bluepic/processRequestThroughCloudantWrite --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy,bluepic/prepareCloudantWrite,bluepic/cloudantWrite -t 300000
+  wsk action create bluepic/processRequestToCloudantWrite --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy,bluepic/prepareCloudantWrite -t 300000
+  wsk action create bluepic/processRequestThroughAlchemy --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather,bluepic/alchemy -t 300000
+  wsk action create bluepic/processRequestThroughWeather --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest,bluepic/weather -t 300000
+  wsk action create bluepic/processRequestToWeather --sequence bluepic/prepareReadImage,bluepic/cloudantRead,bluepic/prepareWeatherRequest -t 300000
+  wsk action create bluepic/processRequestThroughReadImage --sequence bluepic/prepareReadImage,bluepic/cloudantRead -t 300000
+  wsk action create bluepic/processRequestToReadImage --sequence bluepic/prepareReadImage -t 300000
+  wsk action create bluepic/processRequestThroughReadUser --sequence bluepic/prepareReadUser,bluepic/cloudantRead -t 300000
+  wsk action create bluepic/processFinalWrite --sequence bluepic/prepareCloudantWrite,bluepic/cloudantWrite -t 300000
   
   echo -e "${GREEN}Install Complete${NC}"
   wsk list
@@ -91,8 +95,8 @@ function uninstall() {
   wsk action delete bluepic/cloudantWrite
   wsk action delete bluepic/processImage
   wsk action delete bluepic/processImageStub
+  wsk action delete bluepic/kituraCallback
   
-  wsk action delete bluepic/processRequest
   wsk action delete bluepic/processRequestThroughCloudantWrite
   wsk action delete bluepic/processRequestToCloudantWrite
   wsk action delete bluepic/processRequestThroughAlchemy
