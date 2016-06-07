7570811092aac09a46ae1a012b63900b75eba1e4
Taylor Franklin <tlf09a@acu.edu>
2016-06-07 17:48:41 -0500

    populator script fix

--- Cloud-Scripts/populator.sh
+++ Cloud-Scripts/populator.sh
@@ -1,2 +1,2 @@
-./populator.sh --username=<cloudant username> --password=<cloudant password> --projectid=<object storage projectid>
-./populator.sh --userid=<object storage username> --password=<object storage password> --projectid=<object storage projectid>
+./cloudantNoSQLDB/populator.sh --username=<cloudant username> --password=<cloudant password> --projectid=<object storage projectid>
+./Object-Storage/populator.sh --userid=<object storage username> --password=<object storage password> --projectid=<object storage projectid>
