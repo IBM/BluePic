8da67be4dd379a48f19ee1df6649beb52130eff7
Alex Buck <ajbuck@us.ibm.com>
2016-06-08 13:39:41 -0500

    Fixed bug that caused image feed error alert to now show if it fails while the user is still in the login screen

---
+++ BluePic-iOS/BluePic/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,35 @@
+//
+//  UIViewControllerExtension.swift
+//  BluePic
+//
+//  Created by Alex Buck on 6/8/16.
+//  Copyright © 2016 MIL. All rights reserved.
+//
+
+import Foundation
+
+
+extension UIViewController {
+
+
+    /**
+     Method returns whether the view controller is visible or not
+
+     - parameter viewController: UIViewController
+
+     - returns: Bool
+     */
+    func isVisible() -> Bool {
+
+        if let navigationController = self.navigationController, visibleViewController = navigationController.visibleViewController {
+            if self == visibleViewController {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return false
+        }
+
+    }
+}
