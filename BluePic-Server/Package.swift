2e5cb412c9b94c9b4f4bb265b40d6954e449b772
Taylor Franklin <tfranklin@us.ibm.com>
2016-07-27 10:08:59 -0500

    updated package.swift for 06-20

--- BluePic-Server/Package.swift
+++ BluePic-Server/Package.swift
@@ -22,9 +22,9 @@ let package = Package(
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 22),
     .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 0, minor: 22),
     .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 4),
-    .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-swift-sdk.git", majorVersion: 0, minor: 4),
-    .Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-mca-kitura-credentials-plugin.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-pushnotifications-serversdk-swift.git", majorVersion: 0, minor: 2)
+    .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-swift-sdk.git", majorVersion: 0, minor: 5),
+    .Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-mca-kitura-credentials-plugin.git", majorVersion: 0, minor: 3),
+    .Package(url: "https://github.com/ibm-bluemix-mobile-services/bms-pushnotifications-serversdk-swift.git", majorVersion: 0, minor: 3)
   ],
   exclude: ["Makefile", "Kitura-Build"]
 )
