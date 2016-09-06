7c520e693178499e9ba52f6717dd2be1848d7de6
king <king@kings-MacBook-Pro.local>
2016-09-05 19:48:47 -0500

    update search to filter all fields in photos, add tag cloud to homepage

--- BluePic-Server/BluePic-Web/app/components/homepage/homepageController.js
+++ BluePic-Server/BluePic-Web/app/components/homepage/homepageController.js
@@ -9,4 +9,33 @@ angular.module('bluepicWebApp')
         $scope.sortType = 'tags';
         $scope.searchTerm = { value: ""};
 
+        $scope.tags = extractTags();
+
+        function extractTags () {
+            var tagsArray = [], index, tagIndex, photoTags;
+            var photosArray = $scope.photos;
+
+            for (index in photosArray) {
+
+                photoTags = photosArray[index].tags;
+                for (tagIndex in photoTags) {
+
+                    if(tagIsOriginal(photoTags[tagIndex], tagsArray)) {
+                        tagsArray.push(photoTags[tagIndex])
+                    }
+                }
+            }
+            return tagsArray;
+        }
+
+        function tagIsOriginal(tag, tagsArray) {
+            var index;
+
+            for (index in tagsArray) {
+                if (tagsArray[index].label === tag.label){
+                    return false;
+                }
+            }
+            return true;
+        }
 }]);
