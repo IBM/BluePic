8e2c46725aec897084a135e994f87663e39b3122
Taylor Franklin <tfranklin@us.ibm.com>
2017-03-30 09:52:30 -0500

    big update of BluePic dependencies

--- BluePic-Server/Package.swift
+++ BluePic-Server/Package.swift
@@ -30,8 +30,8 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/IBM-Swift/Kitura-CouchDB.git", majorVersion: 1, minor: 6),
-    .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 1),
-    .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 2),
+    .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 2),
+    .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 3),
     .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", majorVersion: 0, minor: 6),
     .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-objectstorage-serversdk-swift.git", majorVersion: 0, minor: 7),
     
