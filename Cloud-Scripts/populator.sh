331cdbe4a37996ecee3765b0e6bc501d3995b277
Aaron Liberatore <AaronLiberatore@aarons-mbp.attlocal.net>
2017-11-02 23:24:49 -0500

    Reverts to boilerplate

--- Cloud-Scripts/populator.sh
+++ Cloud-Scripts/populator.sh
@@ -18,5 +18,5 @@
 
 cloudantFolder=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`/cloudantNoSQLDB
 objectStorageFolder=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`/Object-Storage
-$cloudantFolder/populator.sh --username="d4ae9f62-2765-4fe9-8053-659f0e81928f-bluemix" --password="4ba492fa3866992a0a093b919c74132e01fc99b3b7c6ea70bb83616defa3499b" --projectId="93682ef044ba4b54957ffc96c967b4c9"
-$objectStorageFolder/populator.sh --userId="6a6fdf4bbaf84aec8544698d385e2969" --password="CJ5i6Ff)-Icp)xMi" --projectId="93682ef044ba4b54957ffc96c967b4c9" --region="dallas"
+$cloudantFolder/populator.sh --username="" --password="" --projectId=""
+$objectStorageFolder/populator.sh --userId="" --password="" --projectId="" --region=""
