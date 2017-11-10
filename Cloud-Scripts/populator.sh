c8d1b1290dddcab082d56bc288ff0306da0643ca
Aaron Liberatore <AaronLiberatore@aaron.local>
2017-11-09 20:23:00 -0600

    Removes config

--- Cloud-Scripts/populator.sh
+++ Cloud-Scripts/populator.sh
@@ -18,5 +18,5 @@
 
 cloudantFolder=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`/cloudantNoSQLDB
 objectStorageFolder=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`/Object-Storage
-$cloudantFolder/populator.sh --username="ddad7a02-bf37-4b6e-8d3d-e5c1971697eb-bluemix" --password="390dcb82f98254d397deb397d29d202040d3e9249c3ff17aca6b2d469ae80ed7" --projectId="bf24aff817b2420c863b1abe3c877694"
-$objectStorageFolder/populator.sh --userId="2151033288c1404fa53dc6060b65d28a" --password="azTr/{.9pC07IYR~" --projectId="bf24aff817b2420c863b1abe3c877694" --region="dallas"
+$cloudantFolder/populator.sh --username="" --password="" --projectId=""
+$objectStorageFolder/populator.sh --userId="" --password="" --projectId="" --region=""
